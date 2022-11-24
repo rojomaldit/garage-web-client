8839a0750fcd433b25ca9ee056bf7d4e56e4a269
rojomaldit-jr <marcosgradac2022@gmail.com>
2022-11-23 22:31:15 -0300

    se agregon las pages vehicles y Garages

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,9 @@
 import { Grid } from "@mui/material";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./dashboard";
+import Garages from "./garages";
 import Session from "./session";
+import Vehicles from "./vehicles";
 
 export default function Garage() {
   return (
@@ -10,6 +12,8 @@ export default function Garage() {
         <Routes>
           <Route path="/session" element={<Session />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/vehicles" element={<Vehicles />} />
+          <Route path="/garages" element={<Garages />} />
         </Routes>
       </Router>
     </Grid>
--- src/pages/session/vehicles/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Grid, Typography } from "@mui/material";
-
-export default function Vehicles() {
-    return (
-        <Grid>
-            <Typography variant="h1">sadasdasdasd</Typography>
-        </Grid>
-    )
-}
