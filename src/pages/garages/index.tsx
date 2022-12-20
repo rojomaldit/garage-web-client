9ee87322333a10f88fc6a23a1715917dc9dfb503
rojomaldit-jr <marcosgradac2022@gmail.com>
2022-12-19 23:54:04 -0300

    se agrego garages en service, y se agrego el componente basicTable en garage

--- src/pages/garages/index.tsx
+++ src/pages/garages/index.tsx
@@ -1,11 +1,36 @@
 import { Grid, Typography } from "@mui/material";
-
+import { useEffect, useState } from "react";
+import BasicTable from "../../components/kit/BasicTable";
+import { Garage, getAllGarage } from "../../services/garages";
 import "./Garages.scss";
 
 export default function Garages() {
+  const [garageData, setGarageData] = useState<Garage[]>([]);
+  const handleVehiclesData = () => {
+    (async () => {
+      const data = await getAllGarage();
+      setGarageData(data);
+    })();
+  };
+  useEffect(handleVehiclesData, []);
+
   return (
     <Grid>
-      <Typography variant="h3">Garages</Typography>
+      <Grid>
+        <Typography variant="h3">Garages</Typography>
+      </Grid>
+      <Grid>
+        <BasicTable
+          columns={["lugar", "Creacion", "estado"]}
+          rows={garageData.map((garage) => {
+            return [
+              garage.placeId,
+              garage.createdAt,
+              garage.isAvailable ? "Disponible" : "Ocupado",
+            ];
+          })}
+        />
+      </Grid>
     </Grid>
   );
 }
---
+++ src/services/garages/index.tsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { base, token } from "../VARIABLES";
+
+export type Garage = {
+  id: number
+  createdAt: string ;
+  placeId: string ;
+  isAvailable: string;
+};
+
+export async function getAllGarage() {
+  try {
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    const response = await axios.get(base + "/place-garage", { headers });
+    return response.data;
+  } catch (e) {
+    console.warn(e);
+  }
+}
